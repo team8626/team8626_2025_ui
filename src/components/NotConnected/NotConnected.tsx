a951bfb8fa5fabc37703f6672208640a59b37367
Chris <chris@nautilytics.com>
2025-01-31 16:28:30 -0500

    feat: fix placement of not connected page

--- src/components/NotConnected/NotConnected.tsx
+++ src/components/NotConnected/NotConnected.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react'
 
 const NotConnected: FC = () => {
   return (
-    <div className="flex h-full text-center items-center justify-center">
+    <div className="col-span-full flex h-full w-full text-center items-center justify-center">
       <h1 className="font-black text-8xl text-red-500 tracking-wide">No Connection...</h1>
     </div>
   )
