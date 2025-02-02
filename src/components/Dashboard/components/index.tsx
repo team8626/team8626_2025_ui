a73150c0642b3f77b593fccff76224fea2cad1d7
Cyber Sailors <team8626.sw@gmail.com>
2025-02-02 15:42:39 -0500

    feat: add Timer component and display match time in Dashboard

--- src/components/Dashboard/components/index.tsx
+++ src/components/Dashboard/components/index.tsx
@@ -2,5 +2,6 @@ import LevelButtonGroup, { type Level } from './LevelButtonGroup'
 import IntakeToggle, { type Intake } from './IntakeToggle'
 import StateHistory from './StateHistory'
 import Hexagon from './Hexagon'
+import Timer from './Timer'
 
-export { LevelButtonGroup, IntakeToggle, StateHistory, Level, Intake, Hexagon }
+export { LevelButtonGroup, IntakeToggle, StateHistory, Level, Intake, Hexagon, Timer}
