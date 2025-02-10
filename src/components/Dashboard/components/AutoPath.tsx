4f8f3b9d27606950e975e19f1f93baa6964e98b0
Cyber Sailors <team8626.sw@gmail.com>
2025-02-10 10:04:45 -0500

    feat: add AutoPath component for managing autonomous path selection in the dashboard

---
+++ src/components/Dashboard/components/AutoPath.tsx
@@ -0,0 +1,58 @@
+import type { FC } from 'react';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import useNTValue from '../../../lib/ntcore-react/useNTValue';
+import { NetworkTablesTypeInfos } from 'ntcore-ts-client';
+import { useState, useEffect } from 'react';
+import useNTState from '../../../lib/ntcore-react/useNTState';
+
+const AutoPath: FC = () => {
+  const autoPaths = useNTValue<string[]>(
+    '/SmartDashboard/Auto Path/options',
+    NetworkTablesTypeInfos.kStringArray,
+    ['__ERROR__']
+  );
+  const defaultPath = useNTValue<string>(
+    '/SmartDashboard/Auto Path/default',
+    NetworkTablesTypeInfos.kString,
+    'None'
+  );
+  const [selectedPath,setSelectedPath] = useNTState<string>(
+    '/SmartDashboard/Auto Path/selected',
+    NetworkTablesTypeInfos.kString,
+    defaultPath
+  );
+
+  const [currentPath, setCurrentPath] = useState<string>(selectedPath);
+
+  useEffect(() => {
+    setSelectedPath(defaultPath);
+  }, [defaultPath, setSelectedPath]);
+
+  const handleChange = (event: React.ChangeEvent<{ name?: string; value: string }>) => {
+    console.log(event.target.value);
+    setCurrentPath(event.target.value as string);
+    setSelectedPath(event.target.value as string);
+  };
+
+  return (
+    <div className="flex flex-col gap-y-2 justify-start">
+      <Typography variant="h4">AutoPath</Typography>
+      <Select
+        value={currentPath}
+        variant="outlined"
+        onChange={handleChange}
+        className="w-full bg-white text-black"
+      >
+        {autoPaths.map((path, index) => (
+          <MenuItem key={index} value={path}>
+            {path}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+export default AutoPath;
--- src/components/Dashboard/components/index.tsx
+++ src/components/Dashboard/components/index.tsx
@@ -4,6 +4,7 @@ import StateHistory from './StateHistory'
 import Hexagon from './Hexagon'
 import Timer from './Timer'
 import GamePieceStates from './GamePieceStates'
+import AutoPath from './AutoPath'
 
 
-export { LevelButtonGroup, IntakeToggle, StateHistory, Intake, Hexagon, Timer, GamePieceStates }
+export { LevelButtonGroup, IntakeToggle, StateHistory, Intake, Hexagon, Timer, GamePieceStates, AutoPath }
