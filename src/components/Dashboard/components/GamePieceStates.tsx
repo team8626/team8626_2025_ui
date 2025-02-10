b09ef5625d8cd60a98c3cb6542f35331e4e0b94c
Cyber Sailors <team8626.sw@gmail.com>
2025-02-10 10:04:17 -0500

    feat: refactor GamePieceStates component to use useNTState for state management and enhance UI layout

--- src/components/Dashboard/components/GamePieceStates.tsx
+++ src/components/Dashboard/components/GamePieceStates.tsx
@@ -1,24 +1,48 @@
 import type { FC } from 'react'
 import Typography from '@mui/material/Typography'
+import { NetworkTablesTypeInfos } from 'ntcore-ts-client'
+import useNTState from '../../../lib/ntcore-react/useNTState'
 
-interface GamePieceStates {
-  algaeState: string;
-  coralState: string;
-  algaeShootTime: number;
-  coralShootTime: number;
- }
+const GamePieceStates: FC = () => {
 
-
-const GamePieceStates: FC<GamePieceStates> = ({algaeState, coralState, algaeShootTime, coralShootTime}) => {
-  return (
-    <div className="flex flex-col gap-y-2 justify-start">
-      <Typography variant="h3">Game Pieces</Typography>
-      <Typography variant="body1">ALGAE: {algaeState}</Typography>
-      <Typography variant="body1">ALGAE Last Shoot Time: {algaeShootTime}ms</Typography>
-      <Typography variant="body1">CORAL: {coralState}</Typography>
-      <Typography variant="body1">CORAL Last Shoot Time: {coralShootTime}ms</Typography>
-
-    </div>
+  const [algaeState] = useNTState<string>(
+    '/SmartDashboard/Dashboard/AlgaeState',
+    NetworkTablesTypeInfos.kString,
+    'UNKNOWN'
   )
+  const [coralState] = useNTState<string>(
+    '/SmartDashboard/Dashboard/CoralState',
+    NetworkTablesTypeInfos.kString,
+    'UNKNOWN'
+  )
+
+  const [algaeShootTime] = useNTState<number>(
+    '/SmartDashboard/Subsystems/AlgaeShooter/Last Shot in (ms)',
+    NetworkTablesTypeInfos.kDouble,
+    0
+  )
+  const [coralShootTime] = useNTState<number>(
+    '/SmartDashboard/Subsystems/CoralShooter/Last Shot in (ms)',
+    NetworkTablesTypeInfos.kDouble,
+    0
+  )
+
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-4 justify-start">
+      <Typography variant="h3">Game Pieces</Typography>
+      <div className="flex flex-row gap-x-4">
+        <div className="flex flex-col items-center bg-green-200 p-4 rounded-lg shadow-md text-black">
+          <span className="text-2xl font-bold">ALGAE</span>
+          <span className="text-xl font-bold">{algaeState}</span>
+          <Typography variant="body1">Shoot in: {algaeShootTime}ms</Typography>
+        </div>
+        <div className="flex flex-col items-center bg-red-300 p-4 rounded-lg shadow-md text-black">
+          <span className="text-2xl font-bold">CORAL</span>
+          <span className="text-xl font-bold">{coralState}</span>
+          <Typography variant="body1">Shoot in: {coralShootTime}ms</Typography>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default GamePieceStates
